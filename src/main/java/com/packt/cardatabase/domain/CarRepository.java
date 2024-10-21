3e116f061600334148d8763ca92af17f3dd88fa4
Champ.art.website <32265715+champseo2017@users.noreply.github.com>
2024-10-21 22:27:44 +0700

    Spring Data JPA สร้างคำสั่งค้นหาข้อมูลแบบซับซ้อนได้หลายวิธี

--- src/main/java/com/packt/cardatabase/domain/CarRepository.java
+++ src/main/java/com/packt/cardatabase/domain/CarRepository.java
@@ -2,13 +2,14 @@ package com.packt.cardatabase.domain;
 
 import java.util.List;
 
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.CrudRepository;
 
 import com.packt.cardatabase.entities.Car;  // ควรมีบรรทัดนี้
 
 public interface CarRepository extends CrudRepository<Car, Long> {
      // ค้นหารถยนต์ตามยี่ห้อ
-     List<Car> findByBrand(String brand);
+    //  List<Car> findByBrand(String brand);
 
      // ค้นหารถยนต์ตามสี
      List<Car> findByColor(String color);
@@ -21,4 +22,15 @@ public interface CarRepository extends CrudRepository<Car, Long> {
  
      // ค้นหารถยนต์ตามยี่ห้อหรือสี
      List<Car> findByBrandOrColor(String brand, String color);
+
+      // ค้นหารถยนต์ตามยี่ห้อ และเรียงตามปีผลิตจากน้อยไปมาก
+    List<Car> findByBrandOrderByYearAsc(String brand);
+
+     // ค้นหารถยนต์ตามยี่ห้อโดยใช้ SQL
+    @Query("select c from Car c where c.brand = ?1")
+    List<Car> findByBrand(String brand);
+
+     // ค้นหารถยนต์ที่ยี่ห้อลงท้ายด้วยคำที่กำหนด
+     @Query("select c from Car c where c.brand like %?1")
+     List<Car> findByBrandEndsWith(String brand);
 }
