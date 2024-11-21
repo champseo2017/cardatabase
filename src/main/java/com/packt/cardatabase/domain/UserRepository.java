cf8e3b14dea68718c14fb31bc0e80a5dc608f716
Champ.art.website <32265715+champseo2017@users.noreply.github.com>
2024-11-22 06:49:07 +0700

    ใช้ flag exported ของ annotation @RepositoryRestResource และตั้งค่าเป็น false เพื่อไม่ให้ repository ถูกเปิดเผยเป็น REST resource

--- src/main/java/com/packt/cardatabase/domain/UserRepository.java
+++ src/main/java/com/packt/cardatabase/domain/UserRepository.java
@@ -3,9 +3,11 @@ package com.packt.cardatabase.domain;
 import java.util.Optional;
 
 import org.springframework.data.repository.CrudRepository;
+import org.springframework.data.rest.core.annotation.RepositoryRestResource;
 
-import com.packt.cardatabase.entities.User; 
+import com.packt.cardatabase.entities.User;
 
+@RepositoryRestResource(exported = false)
 public interface UserRepository extends CrudRepository<User, Long> {
     // ใช้ค้นหาผู้ใช้จากฐานข้อมูลโดยใช้ username
     Optional<User> findByUsername(String username);
